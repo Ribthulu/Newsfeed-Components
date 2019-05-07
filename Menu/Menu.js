a1f2b1b59e009b5e35622b94614d19aa0827d6bc
Ribthulu <myemail@example.com>
2019-05-07 19:24:38 -0400

    completed menu MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,26 +1,15 @@
 /*jshint esversion: 6 */
-class Menu{
-  constructor(menu){
-    this.menuButton = this.menu.querySelector(".menu-button");
-    // this.menuOpen = this.menu.querySelector(".menu--open");
-    this.menuButton.addEventListener("click", () => this.toggleMenu());
-  }
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  this.menuButton.classList.toggle("menu--open");
+  menu.classList.toggle("menu--open");
+};
 
-  }
-}
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll(".menu");
+const menu = document.querySelector(".menu");
 console.log(menu);
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelectorAll(".menu-button");
+const menuButton = document.querySelector(".menu-button");
 console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
-
-
-
-
-
-menuButton.addEventListener("click", () => this.toggleMenu());
+menuButton.addEventListener('click', () => toggleMenu());
