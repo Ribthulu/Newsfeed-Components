1962287e8cd6dbc45fe7d85b630373fe2fddeadd
Ribthulu <myemail@example.com>
2019-05-06 23:18:54 -0400

    working on menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,26 @@
-
+/*jshint esversion: 6 */
+class Menu{
+  constructor(menu){
+    this.menuButton = this.menu.querySelector(".menu-button");
+    // this.menuOpen = this.menu.querySelector(".menu--open");
+    this.menuButton.addEventListener("click", () => this.toggleMenu());
+  }
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
+  // Toggle the "menu--open" class on your menu refence.
+  this.menuButton.classList.toggle("menu--open");
 
+  }
+}
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelectorAll(".menu");
+console.log(menu);
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelectorAll(".menu-button");
+console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+
+
+
+
+menuButton.addEventListener("click", () => this.toggleMenu());
